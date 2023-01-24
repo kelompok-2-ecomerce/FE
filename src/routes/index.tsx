5aa7af6d0dfce3f240c62de598b6e602601e39f3
Agung Cahya <agungcahya122@gmail.com>
2023-01-24 20:49:55 +0700

    feat: make aboutme page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,20 +4,28 @@ import {
   Navigate,
 } from "react-router-dom";
 
+import AboutmeProfil from "../pages/aboutmeProfil";
+import AboutmeAlamat from "../pages/aboutmeAlamat";
+import DeactivateAcc from "../pages/DeactivateAcc";
+import DetailBarang from "../pages/DetailBarang";
 import Register from "../pages/auth/register";
 import Transaksi from "../pages/transaksi";
 import Login from "../pages/auth/login";
-import DeactivateAcc from "../pages/DeactivateAcc";
-import DetailBarang from "../pages/DetailBarang";
-
 import Homepage from "../pages/App";
-
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <Homepage />,
+    },
+    {
+      path: "/aboutmeProfil",
+      element: <AboutmeProfil />,
+    },
+    {
+      path: "/aboutmeAlamat",
+      element: <AboutmeAlamat />,
     },
     {
       path: "/transaksi",
@@ -30,18 +38,16 @@ function App() {
     {
       path: "/detailBarang",
       element: <DetailBarang />,
-
     },
     {
       path: "/deactivate",
       element: <DeactivateAcc />,
-      },
-      
-      {
+    },
+
+    {
       path: "/register",
       element: <Register />,
-      }, 
-
+    },
   ]);
 
   return <RouterProvider router={router} />;
