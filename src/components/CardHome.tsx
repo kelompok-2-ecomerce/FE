46bdd131ec24521a7c4ed0d67f12927d7bfd852a
Muhammad Gumilang Tamasakti <gumilangtamasakti@gmail.com>
2023-01-25 15:26:16 +0700

    integrasi: get data api all products

--- src/components/CardHome.tsx
+++ src/components/CardHome.tsx
@@ -3,12 +3,26 @@ import { Link } from "react-router-dom";
 import { useNavigate } from "react-router";
 
 interface CardProps {
-  title: string;
-  price: string;
-  image?: string;
+  id: number;
+  name: string;
+  harga: number;
+  stok: number;
+  description: string;
+  image: string;
+  address: string;
+  penjual: string;
 }
 
-const CardHome: FC<CardProps> = ({ title, price, image }) => {
+const CardHome: FC<CardProps> = ({
+  id,
+  name,
+  harga,
+  stok,
+  description,
+  image,
+  address,
+  penjual,
+}) => {
   const navigate = useNavigate();
 
   function onClickDetail() {
@@ -19,19 +33,35 @@ const CardHome: FC<CardProps> = ({ title, price, image }) => {
       <div className="md:w-11/12 mt-10 ">
         <figure>
           <div className="w-full">
+            <div className="badge border-none bg-green-700 lg:p-3 lg:z-1 lg:mt-4 lg:ml-36  lg:absolute text-white">
+              Stok : {stok}
+            </div>
             <img
               src={image}
-              className="mx-auto rounded-3xl shadow-lg lg:h-64 lg:w-full"
+              className="mx-auto rounded-3xl shadow-lg lg:h-72 lg:w-full"
               onClick={() => onClickDetail()}
             />
           </div>
         </figure>
         <div className="flex flex-row justify-between mt-4">
           <div>
-            <h2 className="text-green-700 font-bold text-lg">{title}</h2>
+            <h2 className="text-green-700 font-bold text-lg">{name}</h2>
+            <p className="hidden">{id}</p>
+
+            <div className="collapse">
+              <input type="checkbox" />
+              <div className="collapse-title text-xl font-medium">
+                <p className="font-bold text-lg">See Details</p>
+              </div>
+              <div className="collapse-content">
+                <p>{description}</p>
+                <p>{address}</p>
+                <p>{penjual}</p>
+              </div>
+            </div>
           </div>
           <div>
-            <p className="text-green-700 font-semibold text-md">{price}</p>
+            <p className="text-green-700 font-semibold text-md">{harga}</p>
           </div>
         </div>
         <button className="btn bg-green-700 border-none text-white font-semibold mt-3 rounded-xl hover:bg-green-900">
